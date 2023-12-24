043c3ee2f6cabd3231e6d47a8781de48e1bd59d2
amma <2534581240@qq.com>
2023-12-24 16:56:41 +0800

    用户数据统计功能业务代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+
 @Mapper
 public interface UserMapper {
     /**
@@ -30,4 +32,21 @@ public interface UserMapper {
      */
     @Select("select *from user where id=#{userId}")
     public User getById(Long userId);
+
+    /**
+     * 查询注册时间范围内的新用户总数
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    @Select("select count(id) from user where create_time between#{beginTime} and #{endTime}")
+    Integer countNewUserByCreateTime(LocalDateTime beginTime, LocalDateTime endTime);
+
+    /**
+     * 查询注册时间范围内的所有用户总数
+     * @param endTime
+     * @return
+     */
+    @Select("select count(id)from user where create_time <= #{endTime}")
+    Integer countTotalUserByCreateTime( LocalDateTime endTime);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     OrderMapper orderMapper;
+
+    @Autowired
+    UserMapper userMapper;
 
     /**
      * 营业额统计
@@ -61,6 +66,43 @@ public class ReportServiceImpl implements ReportService {
         return TurnoverReportVO.builder()
                 .turnoverList(turnoverList1)
                 .dateList(dateList1)
+                .build();
+    }
+
+    /**
+     * 统计指定时间的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //自己计算出日期列表
+        List<LocalDate> dateList = new ArrayList<>();
+        //dateList	string	必须		日期列表，以逗号分隔
+        dateList.add(begin);
+        while (!begin.equals(end)&& begin.isBefore(end))
+        {
+            begin=begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        List<Integer> newUserList=new ArrayList<>();
+        List<Integer> totalUserList=new ArrayList<>();
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime=LocalDateTime.of(date,LocalTime.MIN);
+            LocalDateTime endTime=LocalDateTime.of(date,LocalTime.MAX);
+            //newUserList	string	必须		新增用户数列表，以逗号分隔
+            Integer newUser = userMapper.countNewUserByCreateTime(beginTime,endTime);
+            newUserList.add(newUser);
+            //totalUserList	string	必须		总用户量列表，以逗号分隔
+            Integer totalUser = userMapper.countTotalUserByCreateTime(endTime);
+            totalUserList.add(totalUser);
+        }
+        return UserReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
                 .build();
     }
 }
